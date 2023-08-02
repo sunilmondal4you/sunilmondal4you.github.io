984edbd86be00fc7511f32250ecd1fff42a56256
sunilmondal4you <sunilmondal4you@gmail.com>
2023-08-02 11:48:37 +0530

    skills displayed dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,23 @@ export class AppComponent {
     role: 'Senior Angular Developer | NodeJS Developer',
     discription:
       "I am a Senior Angular Developer with more than 7 years of experience writing Angular, Typescript, Angular JS, Bootstrap, Node JS, HTML, CSS, and JS. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.",
+    skills: [
+      { technology: 'JavaScript', rattings: [true, true, true, true, true] },
+      { technology: 'Angular', rattings: [true, true, true, true, false] },
+      { technology: 'Typescript', rattings: [true, true, true, true, false] },
+      { technology: 'Angular JS', rattings: [true, true, true, true, false] },
+      { technology: 'rxjs', rattings: [true, true, true, true, false] },
+      { technology: 'ngrx', rattings: [true, true, true, true, false] },
+      { technology: 'vue.js', rattings: [true, true, true, true, false] },
+      { technology: 'ionic', rattings: [true, true, true, true, false] },
+      { technology: 'Bootstrap', rattings: [true, true, true, true, false] },
+      // {
+      //   technology: 'Material Design',
+      //   rattings: [true, true, true, true, true],
+      // },
+      { technology: 'Node JS', rattings: [true, true, true, false, false] },
+      { technology: 'Mongodb', rattings: [true, true, true, false, false] },
+      { technology: 'Mysql', rattings: [true, true, true, false, false] },
+    ],
   };
 }
